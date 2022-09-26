4aa8e5a307b1ad2f0d5a343a4f2a98dc027988c8
nieroglyph <reinmark.delrosario@gmail.com>
2022-09-26 09:05:39 +0800

    add final score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // return rock paper or scissors randomly
 function getComputerChoice() {
   let computer = Math.floor(Math.random() * 3);
@@ -13,16 +16,22 @@ function getComputerChoice() {
 // actual round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore++;
         return "You Win! Rock beats Scissors";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerScore++;
         return "You Win! Paper beats Rock";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++;
         return "You Win! Scissors beat Rock";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        computerScore++;
         return "You Lose! Scissors beat Paper";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     } else {
         return "It's a tie!!!";
@@ -31,10 +40,18 @@ function playRound(playerSelection, computerSelection) {
 
 // 5 rounds
 function game() {
-    for (let i = 0; i < 5; i++){
+    while (playerScore < 5){
         const playerSelection = prompt("What u gonna play?".toLowerCase());
         const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection))
+        console.log(playRound(playerSelection, computerSelection));
+
+        if (playerScore === 5) {
+        console.log(`You are a Winner!\nFinal Score = Player - ${playerScore} : Computer - ${computerScore}`) 
+        return false;
+        } else if (computerScore === 5) {
+            console.log(`You are a Loser.\nFinal Score = Player - ${playerScore} : Computer - ${computerScore}`) 
+        return false;
+        }
     }
 }
 
